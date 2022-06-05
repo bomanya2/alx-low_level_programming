5acc0059edb5b6f0d8c370a552ada92f21861185
bomanya2 <bomanya2@gmail.com>
2022-06-05 12:16:53 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  *  * Description: main - Prints out a string to stdout.
  *   * Return: 0 if success.
- *    */
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
