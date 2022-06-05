a8b3fd999c79ad8e4254fd9b9da23f4698b6d6ca
bomanya2 <bomanya2@gmail.com>
2022-06-05 11:10:05 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  * main - Entry point
@@ -16,8 +14,8 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+return (0);
 
 }
