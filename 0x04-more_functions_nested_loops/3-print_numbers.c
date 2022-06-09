18f2ccb21e7dd393a68eaad739be900755128a85
bomanya2 <bomanya2@gmail.com>
2022-06-09 08:36:54 -0700

    function that checks for a digit (0 through 9)

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
-*print_numbers - print the numbers 0 to 9
-*
-*/
+ * print_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: 0
+ */
 void print_numbers(void)
 {
 	int a;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 48; a < 58; a++)
+	{
 		_putchar(a);
+	}
 	_putchar('\n');
 }
