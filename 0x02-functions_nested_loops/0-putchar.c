36f3a2e8bd63e9ba465cbde05005d380680fa0ab
bomanya2 <bomanya2@gmail.com>
2022-06-09 00:47:19 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "alx.h"
+
+/**
+ * main - Print "alx" followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char name[] = "alx";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(name[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
