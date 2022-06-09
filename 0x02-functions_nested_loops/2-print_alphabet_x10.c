3342d56a98bdfd3ff9125ef0c88ff16b5e6531f9
bomanya2 <bomanya2@gmail.com>
2022-06-09 03:28:30 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Print 10 times the alphabet in lowercase.
+ * print_alphabet_x10 - Print 10 times the alphabet, in lowercase.
  */
 void print_alphabet_x10(void)
 {
