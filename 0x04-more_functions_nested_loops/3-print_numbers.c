a7feecdaccb60f7821dca5adb5f022b586aba709
bomanya2 <bomanya2@gmail.com>
2022-06-11 02:25:55 -0700

    Add task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,18 @@
 #include "main.h"
-
 /**
- * print_numbers - print numbers
+ *print_numbers-Prints numbers 0-9.
+ *
+ *Return:void.
  */
-void print_most_numbers(void)
-{
-	int i ;
 
-	for (i = 0; i < 10; x++)
-		_putchar(i + '0' );
+void print_numbers(void)
+{
+	int i;
+
+		for (i = '0'; i <= '9'; i++)
+		{
+
+			_putchar(i);
+		}
 	_putchar('\n');
 }
