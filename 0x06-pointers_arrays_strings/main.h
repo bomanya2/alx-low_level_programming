6cafd5076672d7491317e0ccfc7b4b073a84d94d
bomanya2 <bomanya2@gmail.com>
2022-06-18 02:47:26 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN.H
 #define MAIN_H
-char *_strncat(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
 int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
@@ -10,5 +10,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-char *cap_string(char *);
 #endif
