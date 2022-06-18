c031b6a46d2cd2aaa7e634f79517fd80357eae1b
bomanya2 <bomanya2@gmail.com>
2022-06-18 03:17:11 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,27 @@
+/*
+ * File: 0-strcat.c
+ * Auth: Bomanya2
+ */
+
 #include "main.h"
 
 /**
- * *_strcat - Concatenate two strings
- * @dest: Destiny string.
- * @src: Source string.
- * Return: Destiny spring dest.
+ * strcat - Concatenates the string pointed to by @src, including the terminating
+ *          null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ *
+ * Return: A pointer to the destination string @dest.
  */
-char *_strcat(char *dest, char *src)
+char *strcat(char *dest, const char *src)
 {
-	int index, size;
+	int index = 0, dest_len = 0;
 
-	for (size = 0; dest[size] != '\0'; size++)
-		;
-	for (index = size; src[index - size] != '\0'; index++)
-		dest[index] = src[index - size];
-	dest[index] = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
