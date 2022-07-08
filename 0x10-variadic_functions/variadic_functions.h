c2503699b3bea50cc8fe46beb413fb1a64037096
bomanya2 <bomanya2@gmail.com>
2022-07-08 05:48:46 -0700

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,29 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_c(va_list element);
+void print_i(va_list element);
+void print_f(va_list element);
+void print_s(va_list element);
+
+/**
+ * struct format_t - Struct format_t
+ *
+ * @f: Format.
+ * @print: Function print.
+ */
+typedef struct format_t
+{
+	char *f;
+	void (*print)(va_list);
+} format_t;
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
