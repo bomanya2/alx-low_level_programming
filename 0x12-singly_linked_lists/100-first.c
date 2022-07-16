b1a3da1182ef7f690a342b0ae1b22f576372904f
bomanya2 <bomanya2@gmail.com>
2022-07-16 04:43:37 -0700

    function that prints You're beat! and yet, you must allow,\nI bore my house upon my back

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *startupFun-function executed before main.
+ *
+ *Return:Nothing.
+ */
+void startupFun(void)
+{
+	printf("You're beat! and yet, ");
+	printf("you must allow,\nI bore my house upon my back!\n");
+}
